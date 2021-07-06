ef35993b41d8e3353d094c0262094e2cb17d4f85
Shannon Schrauwen <63632791+ShanSchrauwen@users.noreply.github.com>
2021-07-06 21:21:53 +0300

    creation of react components to be made

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+
+
+
+function Footer(){
+    return(
+        <footer className="footer">
+            hello i am a footer 
+        </footer>
+    )
+
+
+
+}
+
+export {Footer};
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+
+
+function Header(){
+
+    return(
+
+        <header className="header">
+            hello i am a header
+        </header>
+    )
+
+
+}
+
+export {Header};
