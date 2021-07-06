b6f30e2f40381d47d98cfb3faa7d2609874cb8a5
Shannon Schrauwen <63632791+ShanSchrauwen@users.noreply.github.com>
2021-07-06 21:27:04 +0300

    stackoverflow how to parse json added

---
+++ src/components/Orders.jsx
@@ -0,0 +1,4 @@
+// from stackoverflow: how to parse json file
+// https://stackoverflow.com/questions/33650399/es6-modules-implementation-how-to-load-a-json-file/33650470#33650470
+
+// import orders from "../purchaseorders.json"
