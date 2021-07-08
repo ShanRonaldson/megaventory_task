5486215ffebb2935da94505bb2d44a292ee8458d
Shannon Schrauwen <63632791+ShanSchrauwen@users.noreply.github.com>
2021-07-08 13:36:30 +0300

    table of details created

--- src/components/Window.jsx
+++ src/components/Window.jsx
@@ -1,5 +1,5 @@
 import orders from "../data/purchaseorders.json";
-import {Table} from "../components/Table";
+import { Table } from "../components/Table";
 
 export function Window() {
   return (
@@ -10,24 +10,26 @@ export function Window() {
             <section className="window-items">
               <h4 className="window-items__header">Address</h4>
               <p className="window-items__content" key={key}>
-                  {data.PurchaseOrderAddress}
+                {data.PurchaseOrderAddress}
               </p>
             </section>
 
             <section className="window-items">
               <h4 className="window-items__header">Contact Person</h4>
-              <p className="window-items__content" key={key}>{data.PurchaseOrderContactPerson}</p>
+              <p className="window-items__content" key={key}>
+                {data.PurchaseOrderContactPerson}
+              </p>
             </section>
             <section className="window-items">
               <h4 className="window-items__header">Status</h4>
-              <p className="window-items__content" key={key}>{data.PurchaseOrderStatus}</p>
+              <p className="window-items__content" key={key}>
+                {data.PurchaseOrderStatus}
+              </p>
             </section>
             <section className="window-items">
               <h4 className="window-items__header">Details</h4>
-
+              <Table />
             </section>
-
-
           </div>
         );
       })}
